bc360d121696df061b78847214601bbdcf63c49b
angular-cli <angular-cli@angular.io>
2018-03-19 20:23:24 -0500

    chore: initial commit from angular-cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { TechstorePage } from './app.po';
+
+describe('techstore App', function() {
+  let page: TechstorePage;
+
+  beforeEach(() => {
+    page = new TechstorePage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
